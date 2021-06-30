a390a24bbb1041dbe5239bc99a804efceae34d07
ankita <ankita@gmail.com>
2021-06-30 08:18:50 -0700

    components communications, lifecycles, directives, decorators

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,25 +1,31 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, AfterViewInit {
+export class ServerElementComponent implements OnInit, AfterViewInit, AfterViewInit {
   @Input('srvElement')
   element!: { type: string; name: string; content: string; };
   
   @ViewChild('heading')
   header!: ElementRef;
 
+  @ContentChild('contentParagraph')
+  para!: ElementRef;
+
   constructor() {  }
 
   ngOnInit(): void {
-    console.log(this.header);
+    console.log(this.header.nativeElement.textContent);
   }
 
   ngAfterViewInit(){
-    console.log(this.header);
+    console.log(this.header.nativeElement.textContent);
+  }
+  ngAfterContentInit(){
+    console.log(this.para.nativeElement.textContent);
   }
 
 }
